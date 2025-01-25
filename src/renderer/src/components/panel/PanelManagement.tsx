a93ba14318e45af634acf4fe6e7b1fd209c843f1
Mahdi Khajavi <khajavim497@gmail.com>
2025-01-25 09:50:29 +0330

    create this component to display oanels table , cities table and provinces table in one page (panel management page)

--- src/renderer/src/components/panel/PanelManagement.tsx
+++ src/renderer/src/components/panel/PanelManagement.tsx
@@ -1,15 +1,44 @@
-import { Outlet } from "react-router"
+import { Outlet, useLocation } from 'react-router';
+import ProvincesTable from './ProvincesTable';
+import CitiesTable from './CitiesTable';
+import PanelsTable from './PanelsTable';
+import { AnimatePresence, motion } from 'framer-motion';
+import { contentVariants } from '../framer-motion/animations';
 
-function PanelManagement () : JSX.Element {
-    return (
-        <>
-        <div className="flex justify-center items-center mt-4">
-               <h1>مدیریت پنل</h1>
-               <Outlet />
-        </div>
-     
-        </>
-    )
+function PanelManagement(): JSX.Element {
+  const location = useLocation();
+  const isNestedRoute = location.pathname !== '/panel-management';
+
+  return (
+    <div className="w-full h-full p-7 border-1 flex flex-col bg-gray-200">
+      <AnimatePresence>
+        <motion.div
+          variants={contentVariants}
+          initial="exit"
+          animate="enter"
+          exit="exit"
+          className="flex flex-col h-full gap-2"
+        >
+          {/* Check if a nested route is active */}
+          {!isNestedRoute && (
+            <>
+              {/* Render all tables by default */}
+              <div className="grid grid-cols-2 gap-x-2 h-[40%]">
+                <ProvincesTable />
+                <CitiesTable />
+              </div>
+              <div className="flex-1 bg-white rounded-[10px] overflow-auto">
+                <PanelsTable />
+              </div>
+            </>
+          )}
+
+          {/* Render Outlet for nested routes */}
+          {isNestedRoute && <Outlet />}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
 }
 
-export default PanelManagement
+export default PanelManagement;
